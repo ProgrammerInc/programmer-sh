7f5ef74134ed89e06eed3b633911b3ba85bbd43d
James A. Black Jr. <programmer.incorporated@gmail.com>
2025-03-01 17:48:13 -0600

    Fix Terminal Title.

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -104,8 +104,9 @@ const Terminal: React.FC<TerminalProps> = ({
           <div className="w-3 h-3 rounded-full bg-terminal-success" />
         </div>
         <div className="text-terminal-foreground/70 text-sm font-mono flex-1 text-center">
-          <span className="mr-1">&lt;programmer&gt;</span>
+          <span className="mr-1">&lt;programmer&gt;.</span>
           <span className="animate-cursor-blink">_</span>
+          <span className="mr-1">~ portfolio</span>
         </div>
         <div className="w-10"></div> {/* Spacer for symmetry */}
       </div>
