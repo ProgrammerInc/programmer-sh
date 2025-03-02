f4c13ce4be2c89121365fa60fccad254d187237c
James A. Black Jr. <programmer.incorporated@gmail.com>
2025-03-02 01:56:57 +0000

    Fix TS1205 error
    The error "Re-exporting a type when 'isolatedModules' is enabled requires using 'export type'" was resolved by using 'export type' when re-exporting the CommandResult type.

--- src/utils/terminalCommands.ts
+++ src/utils/terminalCommands.ts
@@ -1,4 +1,4 @@
 
 // This file re-exports from the commands directory for backward compatibility
 export { processCommand } from './commands';
-export { CommandResult } from './commands/types';
+export type { CommandResult } from './commands/types';
