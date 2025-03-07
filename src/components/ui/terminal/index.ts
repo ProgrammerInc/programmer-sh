a2680b20dcbb195f007a8d6b0adb8795ed1c6708
James A. Black Jr. <programmer.incorporated@gmail.com>
2025-03-07 06:36:32 +0000

    Fix TS2308 error in index.ts
    The module './terminal' was exporting 'TerminalContent' twice, causing a TypeScript error. Removed the duplicate export to resolve the ambiguity.

--- src/components/ui/terminal/index.ts
+++ src/components/ui/terminal/index.ts
@@ -4,15 +4,15 @@ import TerminalContent from './terminal-content';
 import TerminalFooter from './terminal-footer';
 import * as TerminalUtils from './terminal-utils';
 
+export { Terminal };
+export { TerminalContent };
+export { TerminalFooter };
+export { TerminalUtils };
+
+// Named exports from the component files
 export * from './terminal';
 export * from './terminal-content';
 export * from './terminal-footer';
 export * from './terminal-utils';
-
-export {
-  TerminalContent,
-  TerminalFooter,
-  TerminalUtils
-};
 
 export default Terminal;
