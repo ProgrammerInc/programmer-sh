f3242d1d27c77f479cdade8047747a397dc0620d
James A. Black Jr. <programmer.incorporated@gmail.com>
2025-03-08 18:29:17 +0000

    Reverted to edit c8245904-9a4e-421e-af05-2f8b75d8f212: "Update Resume Command."

--- src/components/ui/terminal/index.ts
+++ src/components/ui/terminal/index.ts
@@ -1,11 +1,10 @@
-
 import Terminal from './terminal';
 import * as TerminalUtils from './terminal-utils';
 
 // Named exports with alias to avoid ambiguity
 export { Terminal, TerminalUtils };
 
-// Export everything except Terminal from component files
+// Export everything from component files
 export * from './terminal';
 export * from './terminal-utils';
 
