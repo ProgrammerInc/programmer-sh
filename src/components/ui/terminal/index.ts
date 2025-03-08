52ce941a2d53a19e485cadc0d14bff4f2f28798e
James A. Black Jr. <programmer.incorporated@gmail.com>
2025-03-08 18:27:47 +0000

    Improve light mode colors
    Improve the readability of the terminal's light mode by adjusting the color scheme.

--- src/components/ui/terminal/index.ts
+++ src/components/ui/terminal/index.ts
@@ -1,10 +1,11 @@
+
 import Terminal from './terminal';
 import * as TerminalUtils from './terminal-utils';
 
 // Named exports with alias to avoid ambiguity
 export { Terminal, TerminalUtils };
 
-// Export everything from component files
+// Export everything except Terminal from component files
 export * from './terminal';
 export * from './terminal-utils';
 
